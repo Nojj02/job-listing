ec85c83933768191bd464898b05fe46b2c251390
Jonn <nojj02@gmail.com>
2020-06-15 23:32:18 +0800

    Added details to the card

--- src/src/Card/component.tsx
+++ src/src/Card/component.tsx
@@ -1,9 +1,18 @@
 import React from "react";
 import "./style.css";
 
-interface CardProps {
+export interface CardProps {
   companyName: string;
-  jobTitle: string;  
+  jobTitle: string;
+  timeSincePosting: string;
+  employmentType: EmploymentType;
+  location: string;
+}
+
+export enum EmploymentType {
+  FullTime = "Full Time",
+  PartTime = "Part Time",
+  Contractual = "Contractual"
 }
 
 const Card = (props: CardProps) => {
@@ -11,6 +20,11 @@ const Card = (props: CardProps) => {
     <div className="Card">
       <div className="company-name">{props.companyName}</div>
       <div className="job-title">{props.jobTitle}</div>
+      <div className="details">
+        <span className="time-since-posting">{props.timeSincePosting}</span>
+        <span className="employment-type">{props.employmentType}</span>
+        <span className="location">{props.location}</span>
+      </div>
     </div>
   );
 };
--- src/src/JobListing/component.tsx
+++ src/src/JobListing/component.tsx
@@ -1,25 +1,44 @@
 import React from "react";
 import "./style.css";
 import Card from "../Card/component";
+import { EmploymentType } from "../Card/component";
 
 const JobListing = () => {
   const cardsContent = [
-    { companyName: "Photosnap", jobTitle: "Senior Frontend Developer" },
-    { companyName: "Manage", jobTitle: "Fullstack Developer" },
-    { companyName: "Account", jobTitle: "Junior Frontend Developer" },
+    {
+      companyName: "Photosnap",
+      jobTitle: "Senior Frontend Developer",
+      timeSincePosting: "1d ago",
+      employmentType: EmploymentType.FullTime,
+      location: "USA only",
+    },
+    {
+      companyName: "Manage",
+      jobTitle: "Fullstack Developer",
+      timeSincePosting: "1d ago",
+      employmentType: EmploymentType.PartTime,
+      location: "Remote",
+    },
+    {
+      companyName: "Account",
+      jobTitle: "Junior Frontend Developer",
+      timeSincePosting: "2d ago",
+      employmentType: EmploymentType.PartTime,
+      location: "USA only",
+    },
   ];
 
   const cards = cardsContent.map((cardContent) => (
-    <Card companyName={cardContent.companyName}
+    <Card
+      companyName={cardContent.companyName}
       jobTitle={cardContent.jobTitle}
+      timeSincePosting={cardContent.timeSincePosting}
+      employmentType={cardContent.employmentType}
+      location={cardContent.location}
     />
   ));
 
-  return (
-    <div className="JobListing">
-      {cards}
-    </div>
-  );
+  return <div className="JobListing">{cards}</div>;
 };
 
 export default JobListing;
