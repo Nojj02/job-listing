fdc3127af3675c74ee6e2911d45e362143eb9ff4
Jonn <nojj02@gmail.com>
2020-08-31 17:48:42 +0800

    feat: Added card callouts; moved to testdata to its own file

--- src/src/Card/component.tsx
+++ src/src/Card/component.tsx
@@ -10,6 +10,8 @@ export interface CardProps {
   location: string;
   role: string;
   level: string;
+  isNew: boolean | undefined;
+  isFeatured: boolean | undefined;
   languages: string[] | undefined;
   tools: string[] | undefined;
 }
@@ -26,20 +28,38 @@ const Card = (props: CardProps) => {
   tags.push(props.level);
   tags.push(...(props.languages || []));
   tags.push(...(props.tools || []));
+  
   const tagComponents = [];
   for (const tag of tags) {
     tagComponents.push(<div className="tag">{tag}</div>);
   }
+
   const imageSrc = require(`../assets/logos/${props.companyCode}.svg`);
 
+  let cardClass = "Card";
+  if (props.isFeatured) {
+    cardClass += " card-featured";
+  }
+
+  const cardCallouts : JSX.Element[] = [];
+  if (props.isNew) {
+    cardCallouts.push(<div className="card-callout card-new">new!</div>);
+  }
+  if (props.isFeatured) {
+    cardCallouts.push(<div className="card-callout card-featured">featured</div>);
+  }
+
   return (
-    <div className="Card">
+    <div className={cardClass}>
       <div className="left">
         <div className="company-image">
           <img src={imageSrc} />
         </div>
         <div className="company-info">
-          <div className="company-name">{props.companyName}</div>        
+          <div className="company-info-head">
+            <div className="company-name">{props.companyName}</div>
+            {cardCallouts}
+          </div>
           <div className="job-title">{props.jobTitle}</div>
           <div className="details">
             <span className="time-since-posting">{props.timeSincePosting}</span>
--- src/src/JobListing/component.tsx
+++ src/src/JobListing/component.tsx
@@ -3,140 +3,27 @@ import "./style.css";
 import Card from "../Card/component";
 import { EmploymentType } from "../Card/component";
 
-const JobListing = () => {
-  const cardsContent = [
-    {
-      companyName: "Photosnap",
-      companyCode: "photosnap",
-      jobTitle: "Senior Frontend Developer",
-      timeSincePosting: "1d ago",
-      employmentType: EmploymentType.FullTime,
-      location: "USA only",
-      role: "Frontend",
-      level: "Senior",
-      languages: ["HTML", "CSS", "JavaScript"],
-    },
-    {
-      companyName: "Manage",
-      companyCode: "manage",
-      jobTitle: "Fullstack Developer",
-      timeSincePosting: "1d ago",
-      employmentType: EmploymentType.PartTime,
-      location: "Remote",
-      role: "Fullstack",
-      level: "Midweight",
-      languages: ["Python"],
-      tools: ["React"],
-    },
-    {
-      companyName: "Account",
-      companyCode: "account",
-      jobTitle: "Junior Frontend Developer",
-      timeSincePosting: "2d ago",
-      employmentType: EmploymentType.PartTime,
-      location: "USA only",
-      role: "Frontend",
-      level: "Senior",
-      languages: ["JavaScript"],
-      tools: ["React", "Sass"],
-    },
-    {
-      companyName: "MyHome",
-      companyCode: "myhome",
-      jobTitle: "Junior Frontend Developer",
-      timeSincePosting: "5d ago",
-      employmentType: EmploymentType.Contractual,
-      location: "USA only",
-      role: "Frontend",
-      level: "Junior",
-      languages: ["CSS", "JavaScript"],
-    },
-    {
-      companyName: "Loop Studios",
-      companyCode: "loop-studios",
-      jobTitle: "Software Engineer",
-      timeSincePosting: "1w ago",
-      employmentType: EmploymentType.FullTime,
-      location: "Worldwide",
-      role: "Fullstack",
-      level: "Midweight",
-      languages: ["JavaScript", "Ruby"],
-      tools: ["Sass"],
-    },
-    {
-      companyName: "FaceIt",
-      companyCode: "faceit",
-      jobTitle: "Junior Backend Developer",
-      timeSincePosting: "2w ago",
-      employmentType: EmploymentType.FullTime,
-      location: "UK only",
-      role: "Backend",
-      level: "Junior",
-      languages: ["Ruby"],
-      tools: ["RoR"],
-    },
-    {
-      companyName: "Shortly",
-      companyCode: "shortly",
-      jobTitle: "Junior Developer",
-      timeSincePosting: "2w ago",
-      employmentType: EmploymentType.FullTime,
-      location: "Worldwide",
-      role: "Frontend",
-      level: "Junior",
-      languages: ["HTML", "JavaScript"],
-      tools: ["Sass"],
-    },
-    {
-      companyName: "Insure",
-      companyCode: "insure",
-      jobTitle: "Junior Frontend Developer",
-      timeSincePosting: "2w ago",
-      employmentType: EmploymentType.FullTime,
-      location: "USA only",
-      role: "Backend",
-      level: "Junior",
-      languages: ["JavaScript"],
-      tools: ["Vue", "Sass"],
-    },
-    {
-      companyName: "Eyecam Co.",
-      companyCode: "eyecam-co",
-      jobTitle: "Full Stack Engineer",
-      timeSincePosting: "3w ago",
-      employmentType: EmploymentType.FullTime,
-      location: "Worldwide",
-      role: "Fullstack",
-      level: "Midweight",
-      languages: ["JavaScript", "Python"],
-      tools: ["Django"],
-    },
-    {
-      companyName: "The Air Filter Company",
-      companyCode: "the-air-filter-company",
-      jobTitle: "Front-end Dev",
-      timeSincePosting: "1mo ago",
-      employmentType: EmploymentType.PartTime,
-      location: "Worldwide",
-      role: "Frontend",
-      level: "Junior",
-      languages: ["JavaScript"],
-      tools: ["React", "Sass"],
-    },
-  ];
+import * as CardsContent from "../testData.json";
 
-  const cards = cardsContent.map((cardContent) => (
+const JobListing = () => {
+  const cards = CardsContent.jobs.map((cardContent) => (
     <Card
       companyName={cardContent.companyName}
       companyCode={cardContent.companyCode}
       jobTitle={cardContent.jobTitle}
       timeSincePosting={cardContent.timeSincePosting}
-      employmentType={cardContent.employmentType}
+      employmentType={
+        EmploymentType[
+          cardContent.employmentType as keyof typeof EmploymentType
+        ]
+      }
       location={cardContent.location}
       role={cardContent.role}
       level={cardContent.level}
       languages={cardContent.languages}
       tools={cardContent.tools}
+      isFeatured={cardContent.isFeatured}
+      isNew={cardContent.isNew}
     />
   ));
 
